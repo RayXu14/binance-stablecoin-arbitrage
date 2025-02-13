1e9d768820c1c0456cb71074c7b9799956bd6e3d
Ray Xu <22774575+RayXu14@users.noreply.github.com>
2025-02-14 00:33:16 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Recommended to run in a tmux session to avoid interruption (I know it's weird but ...)
-nohup python -u main.py --base_asset FDUSD --initial_quote 1000 --buy_price 0.9998 > logs/1.log 2>&1 &
-nohup python -u main.py --base_asset FDUSD --initial_quote 1000 --buy_price 0.9999 > logs/2.log 2>&1 &
-nohup python -u main.py --base_asset FDUSD --initial_quote 1000 --buy_price 1.0000 > logs/3.log 2>&1 &
-nohup python -u main.py --base_asset FDUSD --initial_quote 1000 --buy_price 1.0001 > logs/4.log 2>&1 &
+# nohup python -u main.py --base_asset FDUSD --initial_quote 1000 --buy_price 0.9998 > logs/1.log 2>&1 &
+# nohup python -u main.py --base_asset FDUSD --initial_quote 1000 --buy_price 0.9999 > logs/2.log 2>&1 &
+# nohup python -u main.py --base_asset FDUSD --initial_quote 1000 --buy_price 1.0000 > logs/3.log 2>&1 &
+# nohup python -u main.py --base_asset FDUSD --initial_quote 1000 --buy_price 1.0001 > logs/4.log 2>&1 &
 nohup python -u main.py --base_asset USDC --initial_quote 1000 --buy_price 0.9998 > logs/5.log 2>&1 &
 nohup python -u main.py --base_asset USDC --initial_quote 1000 --buy_price 0.9999 > logs/6.log 2>&1 &
 nohup python -u main.py --base_asset USDC --initial_quote 1000 --buy_price 1.0000 > logs/7.log 2>&1 &
